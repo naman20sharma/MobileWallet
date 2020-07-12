7b93d7962e250f39a8cab181468a78f6689ef6be
Naman <naman20sharma@gmail.com>
2020-07-12 11:40:45 +0530

    Update SplashActivity.cs
    Changed Intent settings in Splash for Triggering the event of LocalNotificationTapped

--- MyWallet/MyWallet.Android/SplashActivity.cs
+++ MyWallet/MyWallet.Android/SplashActivity.cs
@@ -1,5 +1,6 @@
 ﻿
 using Android.App;
+using Android.Content;
 using Android.OS;
 
 namespace MyWallet.Droid
@@ -7,13 +8,20 @@ namespace MyWallet.Droid
     [Activity(Theme = "@style/Splash", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : Activity
     {
+        private static readonly string ExtraReturnDataAndroid = "Plugin.LocalNotification.RETURN_DATA";
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
-            StartActivity(typeof(MainActivity));
-            Finish();
+            var data = Intent.GetStringExtra(ExtraReturnDataAndroid);
+            var intent = new Intent(Application.Context, typeof(MainActivity));
+            if(!string.IsNullOrWhiteSpace(data))
+            {
+                intent.PutExtra(ExtraReturnDataAndroid, data);
+            }
+            StartActivity(intent);
+            //Finish();
         }
     }
 }
